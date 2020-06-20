4bfc8f605ac21cfb0575ea2a9edfdd7f4595a4c4
NesterenkoII <ii.nesterenko78@gmail.com>
2020-06-20 23:07:14 +0300

    lesson07 modern UI

--- ru/nesterenko/part01/lesson07/task01/VendingMachine/Menu.java
+++ ru/nesterenko/part01/lesson07/task01/VendingMachine/Menu.java
@@ -13,11 +13,11 @@ public class Menu {
         System.out.println("");
         System.out.println("_________________________________________________");
         System.out.println("|   Dыберите пункт меню и нажмите \"Enter\"       |");
-        System.out.println("|_______________________________________________|");
+        System.out.println("|===============================================|");
         System.out.println("|                    МЕНЮ                       |");
-        System.out.println("|_______________________________________________|");
+        System.out.println("|----------------+----------------+-------------|");
         System.out.println("|Наименование    |   Цена, руб.   | Остаток, шт |");
-        System.out.println("|_______________________________________________|");
+        System.out.println("|----------------+----------------+-------------|");
         System.out.println("| 1 - Фанта      |   ( " + vendingMachine.drinksArray.get(0).getPrice() + " )     |     "
                             + vendingMachine.drinksArray.get(0).getCount() + "       |");
         System.out.println("| 2 - Кока-Кола  |   ( " + vendingMachine.drinksArray.get(1).getPrice() + " )     |     "
@@ -27,8 +27,9 @@ public class Menu {
         System.out.println("|_______________________________________________|");
         System.out.println("| 4 - Пополнить баланс                          |");
         System.out.println("| 0 - Выход                                     |");
-        System.out.println("|_______________________________________________|");
+        System.out.println("|===============================================|");
         System.out.println("            Текущий баланс: " + vendingMachine.getDEPOSIT() + " рублей                 ");
-        System.out.println("|_______________________________________________|");
+        System.out.println("|===============================================|");
+        System.out.print("                  Ваш выбор - " );
     }
 }
